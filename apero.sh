d69a1d6a0a55864d5b47a5c235ef56ff71a5bfd9
C-A Paquet <charles-antoine.paquet@acasi.io>
2021-09-09 15:22:30 +0200

    duree script

--- apero.sh
+++ apero.sh
@@ -3,11 +3,11 @@ declare -a arr=("hey.mp3" "cough.mp3" "keyboard.mp3" "sheep.mp3" "cat.mp3")
 while true
 do	
 	OLD=$(osascript -e 'set ovol to output volume of (get volume settings)')
-	osascript -e 'set volume output volume 50'
+	osascript -e 'set volume output volume 1'
 
 	SOUND=$(($RANDOM % 5))
 	afplay ${arr[($SOUND)]}
 
 	osascript -e "set volume output volume $OLD"
-	sleep $((1 + $RANDOM % 5))
+	sleep $((1 + $RANDOM % 600))
 done
