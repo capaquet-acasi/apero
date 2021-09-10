cc09459b80c448558558ab1f6e373197ae00021c
C-A Paquet <charles-antoine.paquet@acasi.io>
2021-09-10 20:31:42 +0200

    variable names + soundlevel

--- apero.sh
+++ apero.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-declare -a arr=("hey.mp3" "cough.mp3" "keyboard.mp3" "sheep.mp3" "cat.mp3")
+declare -a SOUND_LIST=("hey.mp3" "cough.mp3" "keyboard.mp3" "sheep.mp3" "cat.mp3")
 while true
 do	
 	OLD=$(osascript -e 'set ovol to output volume of (get volume settings)')
-	osascript -e 'set volume output volume 1'
+	osascript -e 'set volume output volume 30'
 
-	SOUND=$(($RANDOM % 5))
-	afplay ${arr[($SOUND)]}
+	SOUND_INDEX=$(($RANDOM % 5))
+	afplay ${SOUND_LIST[($SOUND_INDEX)]}
 
 	osascript -e "set volume output volume $OLD"
-	sleep $((1 + $RANDOM % 600))
+	sleep $((1 + $RANDOM % 5))
 done
